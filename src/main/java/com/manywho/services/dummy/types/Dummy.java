a64c947ab80de7a6158893b8a57e43dca46676de
Jonjo McKay <jonjo.mckay@manywho.com>
2015-05-08 16:32:46 +0100

    Added a dummy type with bindings

---
+++ src/main/java/com/manywho/services/dummy/types/Dummy.java
@@ -0,0 +1,30 @@
+package com.manywho.services.dummy.types;
+
+import com.manywho.sdk.entities.draw.elements.type.*;
+import com.manywho.sdk.enums.ContentType;
+import com.manywho.sdk.services.describe.types.AbstractType;
+
+public class Dummy extends AbstractType {
+    public final static String NAME = "Dummy";
+
+    @Override
+    public String getDeveloperName() {
+        return NAME;
+    }
+
+    @Override
+    public TypeElementBindingCollection getBindings() {
+        return new TypeElementBindingCollection() {{
+            add(new TypeElementBinding("Dummy", "Dummy bindings", "dummy", new TypeElementPropertyBindingCollection() {{
+                add(new TypeElementPropertyBinding("Name", "Name"));
+            }}));
+        }};
+    }
+
+    @Override
+    public TypeElementPropertyCollection getProperties() {
+        return new TypeElementPropertyCollection() {{
+            add(new TypeElementProperty("Name", ContentType.String));
+        }};
+    }
+}
