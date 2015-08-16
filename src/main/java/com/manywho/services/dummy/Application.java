aeb53073edb4ad5be6a820cb927f827f7bc1f0fd
Jonjo McKay <jonjo@jonjomckay.com>
2015-08-16 10:32:53 +0100

    Replaced WAR packaging with a runnable JAR based on Grizzly

--- src/main/java/com/manywho/services/dummy/Application.java
+++ src/main/java/com/manywho/services/dummy/Application.java
@@ -1,12 +1,36 @@
 package com.manywho.services.dummy;
 
 import com.manywho.sdk.services.BaseApplication;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 
 import javax.ws.rs.ApplicationPath;
+import java.io.IOException;
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ApplicationPath("/")
 public class Application extends BaseApplication {
+    private static final URI BASE_URI = URI.create("http://localhost:8080/");
+
     public Application() {
         registerSdk().packages("com.manywho.services.dummy");
+    }
+
+    public static void main(String[] args) {
+        try {
+            final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, new Application(), false);
+
+            Runtime.getRuntime().addShutdownHook(new Thread(server::shutdownNow));
+
+            server.start();
+
+            System.out.println(String.format("Application started.\nTry out %s\nStop the application using CTRL+C", BASE_URI));
+
+            Thread.currentThread().join();
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
