f3cbba17f58a56c5dd96672735e69e9d728b0e9a
Jonjo McKay <jonjo@jonjomckay.com>
2019-04-11 17:26:19 +0100

    Fixed authentication callback redirect URI

--- src/main/java/com/manywho/services/dummy/auth/FakeIdProvider.java
+++ src/main/java/com/manywho/services/dummy/auth/FakeIdProvider.java
@@ -4,7 +4,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @Path("/callback")
 public class FakeIdProvider {
@@ -12,9 +15,11 @@ public class FakeIdProvider {
     @GET
     public Response fakeIdProvider(
             @QueryParam("state") String state,
-            @QueryParam("redirect_uri") String redirectUri) {
+            @QueryParam("redirect_uri") String redirectUri) throws UnsupportedEncodingException {
 
-        String url = String.format("%s?code=1234&state=%s&redirect_uri=%s", redirectUri, state, redirectUri);
+        String decodedRedirectUri = URLDecoder.decode(redirectUri, StandardCharsets.UTF_8.toString());
+
+        String url = String.format("%s?code=1234&state=%s&redirect_uri=%s", decodedRedirectUri, state, decodedRedirectUri);
 
         return Response.seeOther(URI.create(url)).build();
     }
