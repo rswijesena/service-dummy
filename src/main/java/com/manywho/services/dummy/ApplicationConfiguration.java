05a19a651088053c1d0e28b702563a7e90383c0a
Dom Holmes <dom_holmes@dell.com>
2019-06-11 15:12:47 +0100

    feature/CORE-5273: Adding configuration values to aid in testing

--- src/main/java/com/manywho/services/dummy/ApplicationConfiguration.java
+++ src/main/java/com/manywho/services/dummy/ApplicationConfiguration.java
@@ -1,6 +1,13 @@
 package com.manywho.services.dummy;
 
+import com.manywho.sdk.api.ContentType;
 import com.manywho.sdk.services.configuration.Configuration;
 
 public class ApplicationConfiguration implements Configuration {
+
+    @Configuration.Setting(name = "Hostname", contentType = ContentType.String, required = true)
+    private String username;
+
+    @Configuration.Setting(name = "Port", contentType = ContentType.Number, required = false)
+    private String password;
 }
