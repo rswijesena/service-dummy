e5527ec0f488ef32e24450399d0b51b28bb4c21c
Dom Holmes <dom_holmes@dell.com>
2019-07-18 10:13:03 +0100

    CORE-5146: Added controller to return invalid responses

--- src/main/java/com/manywho/services/dummy/dummy/InvalidResponseController.java
+++ src/main/java/com/manywho/services/dummy/dummy/InvalidResponseController.java
@@ -5,8 +5,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import java.io.UnsupportedEncodingException;
 
@@ -15,9 +13,6 @@ import java.io.UnsupportedEncodingException;
 @Produces(MediaType.APPLICATION_JSON)
 public class InvalidResponseController {
     
-    @Context
-    private HttpHeaders httpHeaders;
-
     @Path("/invaliddate")
     @POST
     public Response invalid() throws UnsupportedEncodingException {
