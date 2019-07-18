32f07c03a72cf5c88f4e1cbdd39b34152de93512
Dom Holmes <dom_holmes@dell.com>
2019-07-18 11:58:41 +0100

    CORE-5146: Added controller to return invalid responses

--- src/main/java/com/manywho/services/dummy/dummy/InvalidResponseController.java
+++ src/main/java/com/manywho/services/dummy/dummy/InvalidResponseController.java
@@ -1,22 +1,53 @@
 package com.manywho.services.dummy.dummy;
 
+import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import com.manywho.sdk.api.ContentType;
+import com.manywho.sdk.api.InvokeType;
+import com.manywho.sdk.api.run.EngineValue;
+import com.manywho.sdk.api.run.elements.config.ServiceResponse;
+import com.manywho.sdk.api.run.elements.type.ObjectDataRequest;
+import com.manywho.sdk.api.security.AuthenticatedWho;
+
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @Path("/invalidresponse")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class InvalidResponseController {
     
+    private final Provider<AuthenticatedWho> authenticatedWhoProvider;
+
+    @Inject
+    public InvalidResponseController(Provider<AuthenticatedWho> authenticatedWhoProvider) {
+        this.authenticatedWhoProvider = authenticatedWhoProvider;
+    }
+    
+    
     @Path("/invaliddate")
     @POST
-    public Response invalid() throws UnsupportedEncodingException {
+    public Response invalid(ObjectDataRequest objectDataRequest) throws UnsupportedEncodingException {
 
-        return Response.ok("{\"annotations\":{},\"culture\":null,\"invokeType\":\"FORWARD\",\"mode\":null,\"outputs\":[{\"contentType\":\"ContentDateTime\",\"contentValue\":\"NotAValidDate\",\"developerName\":\"Body\",\"objectData\":[],\"typeElementDeveloperName\":null,\"typeElementId\":null,\"typeElementPropertyDeveloperName\":null,\"typeElementPropertyId\":null,\"valueElementId\":null}],\"rootFaults\":{},\"selectedOutcomeId\":null,\"tenantId\":\"65e49be8-c8b4-4b5d-b2d2-a52e95ccc307\",\"token\":\"d07b2784-bf8a-4879-8b22-6f32bb482876\",\"valueFaults\":[],\"waitMessage\":null}").build();
+        AuthenticatedWho authenticatedWho = authenticatedWhoProvider.get();
+        ServiceResponse response = new ServiceResponse();
+        response.setToken(objectDataRequest.getToken());
+        response.setTenantId(authenticatedWho.getManyWhoTenantId());
+        response.setInvokeType(InvokeType.Forward);
+        
+        EngineValue value1 = new EngineValue();
+        value1.setContentType(ContentType.DateTime);
+        value1.setContentValue("NotAValidDate"); 
+        value1.setDeveloperName("Body");
+        response.setOutputs(List.of(value1));
+        
+        return Response.ok(response).build();
     }
 }
