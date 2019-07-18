0134be4cdf1ba93172dfbd43c2344abf3fc1f628
Dom Holmes <dom_holmes@dell.com>
2019-07-18 09:05:07 +0100

    CORE-5146: Added controller to return invalid responses

--- src/main/java/com/manywho/services/dummy/dummy/InvalidResponseController.java
+++ src/main/java/com/manywho/services/dummy/dummy/InvalidResponseController.java
@@ -1,14 +1,20 @@
 package com.manywho.services.dummy.dummy;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import java.io.UnsupportedEncodingException;
 
 @Path("/invalidresponse")
 public class InvalidResponseController {
+    
+    @Context
+    private HttpHeaders httpHeaders;
+
     @Path("/invaliddate")
-    @GET
+    @POST
     public Response invalid() throws UnsupportedEncodingException {
 
         return Response.ok("{\"annotations\":{},\"culture\":null,\"invokeType\":\"FORWARD\",\"mode\":null,\"outputs\":[{\"contentType\":\"ContentDateTime\",\"contentValue\":\"NotAValidDate\",\"developerName\":\"Body\",\"objectData\":[],\"typeElementDeveloperName\":null,\"typeElementId\":null,\"typeElementPropertyDeveloperName\":null,\"typeElementPropertyId\":null,\"valueElementId\":null}],\"rootFaults\":{},\"selectedOutcomeId\":null,\"tenantId\":\"65e49be8-c8b4-4b5d-b2d2-a52e95ccc307\",\"token\":\"d07b2784-bf8a-4879-8b22-6f32bb482876\",\"valueFaults\":[],\"waitMessage\":null}").build();
