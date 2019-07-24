aac73b8cdba85edca59494942039d3986a837456
Dom Holmes <dom_holmes@dell.com>
2019-07-24 15:13:46 +0100

    CORE-5146: Added invalidlist dummy endpoint

--- src/main/java/com/manywho/services/dummy/dummy/InvalidResponseController.java
+++ src/main/java/com/manywho/services/dummy/dummy/InvalidResponseController.java
@@ -13,6 +13,7 @@ import com.manywho.sdk.api.ContentType;
 import com.manywho.sdk.api.InvokeType;
 import com.manywho.sdk.api.run.EngineValue;
 import com.manywho.sdk.api.run.elements.config.ServiceResponse;
+import com.manywho.sdk.api.run.elements.type.MObject;
 import com.manywho.sdk.api.run.elements.type.ObjectDataRequest;
 import com.manywho.sdk.api.security.AuthenticatedWho;
 
@@ -51,6 +52,41 @@ public class InvalidResponseController {
         
         List<EngineValue> values = new ArrayList<EngineValue>();
         values.add(invalidDateValue);
+        response.setOutputs(values);
+        
+        return Response.ok(response).build();
+    }
+
+    @Path("/invalidlist")
+    @POST
+    public Response invalidList(ObjectDataRequest objectDataRequest) throws UnsupportedEncodingException {
+
+        AuthenticatedWho authenticatedWho = authenticatedWhoProvider.get();
+        
+        ServiceResponse response = new ServiceResponse();
+        response.setToken(objectDataRequest.getToken());
+        response.setTenantId(authenticatedWho.getManyWhoTenantId());
+        response.setInvokeType(InvokeType.Forward);
+        
+        EngineValue invalidList = new EngineValue();
+        invalidList.setContentType(ContentType.List);
+        
+        List<MObject> listObjectData = new ArrayList<MObject>();
+        
+        MObject firstItem = new MObject();
+        firstItem.setOrder(1);
+        firstItem.setSelected(true);
+        listObjectData.add(firstItem);
+        
+        MObject secondItem = new MObject();
+        secondItem.setOrder(1);
+        secondItem.setSelected(true);
+        listObjectData.add(secondItem);
+
+        invalidList.setObjectData(listObjectData);
+
+        List<EngineValue> values = new ArrayList<EngineValue>();
+        values.add(invalidList);
         response.setOutputs(values);
         
         return Response.ok(response).build();
