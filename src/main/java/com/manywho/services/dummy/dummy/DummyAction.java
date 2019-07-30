7d13301f7122143212dfcab6989c28cc500cfadc
Dom Holmes <dom_holmes@dell.com>
2019-07-30 09:59:02 +0100

    Revert "CORE-5273: Added action to fail validation"
    This reverts commit 98349560fcc129668118c0d661a3504a85ca2a26.

--- src/main/java/com/manywho/services/dummy/dummy/DummyAction.java
+++ src/main/java/com/manywho/services/dummy/dummy/DummyAction.java
@@ -1,8 +1,5 @@
 package com.manywho.services.dummy.dummy;
 
-import java.time.OffsetDateTime;
-import java.util.List;
-
 import com.manywho.sdk.api.ContentType;
 import com.manywho.sdk.services.actions.Action;
 
@@ -25,23 +22,15 @@ public class DummyAction implements Action {
     }
 
     public static class Output {
-        @Action.Output(name = "Date", contentType = ContentType.DateTime)
-        private OffsetDateTime date;
+        @Action.Output(name = "Body", contentType = ContentType.String)
+        private String body;
 
-        @Action.Output(name = "Modes", contentType = ContentType.List)
-        private List<String> modes;
-
-        public Output(OffsetDateTime date, List<String> modes) {
-            this.date = date;
-            this.modes = modes;
+        public Output(String body) {
+            this.body = body;
         }
 
-        public OffsetDateTime getDate() {
-            return date;
-        }
-
-        public List<String> getModes() {
-            return modes;
+        public String getBody() {
+            return body;
         }
     }
 }
--- src/main/java/com/manywho/services/dummy/dummy/DummyActionCommand.java
+++ src/main/java/com/manywho/services/dummy/dummy/DummyActionCommand.java
@@ -1,8 +1,5 @@
 package com.manywho.services.dummy.dummy;
 
-import java.time.OffsetDateTime;
-import java.util.Arrays;
-
 import com.manywho.sdk.api.InvokeType;
 import com.manywho.sdk.api.run.elements.config.ServiceRequest;
 import com.manywho.sdk.services.actions.ActionCommand;
@@ -14,7 +11,7 @@ import com.manywho.services.dummy.dummy.DummyAction.Output;
 public class DummyActionCommand implements ActionCommand<ApplicationConfiguration, DummyAction, Input, Output> {
     @Override
     public ActionResponse<Output> execute(ApplicationConfiguration configuration, ServiceRequest request, Input input) {
-        Output output = new Output(OffsetDateTime.now(), Arrays.asList("full", "partial", "complete"));
+        Output output = new Output("Hello from the Dummy Service");
 
         return new ActionResponse<>(output, InvokeType.Forward);
     }
