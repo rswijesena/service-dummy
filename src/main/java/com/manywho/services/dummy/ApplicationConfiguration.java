f46460b6f904c5efde63d557c8714726761d7fdc
Jose Collazzi <josecollazzi@gmail.com>
2019-08-29 12:03:29 +0100

    issue #7 making include type with bindings not mandatory

--- src/main/java/com/manywho/services/dummy/ApplicationConfiguration.java
+++ src/main/java/com/manywho/services/dummy/ApplicationConfiguration.java
@@ -5,7 +5,7 @@ import com.manywho.sdk.services.configuration.Configuration;
 
 public class ApplicationConfiguration implements Configuration {
 
-    @Configuration.Setting(name = "Include Types With Bindings", contentType = ContentType.Boolean)
+    @Configuration.Setting(name = "Include Types With Bindings", contentType = ContentType.Boolean, required = false)
     private boolean includeTypesWithBindings;
 
     public boolean getIncludeTypesWithBindings() {
